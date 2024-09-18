39ce18403ecc70d90c55c86a6502d65a4ea240a4
ramiro.couceiro <ramiro.couceiro@inxpirius.com>
2024-09-18 15:08:44 -0300

    fix

--- app/Sidenav/Sidenav.tsx
+++ app/Sidenav/Sidenav.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useTheme } from '../ThemeContext';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 import {
